2c8465437fa34ebf7fec95c6c97ffdd872b8711f
Romain <romain@nutrinomics.care>
2023-03-08 15:35:48 +0100

    init app

---
+++ .history/app_20230308151934.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.json());
+app.post('/', (req, res)=>{
+    const {name} = req.body;
+      
+    res.send(`Welcome ${name}`);
+})
+
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.header('Access-Control-Allow-Origin: *');
+    res.send("welcome to root URL of Server");
+});
+
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@cluster0.oby0lcp.mongodb.net/?retryWrites=true&w=majority`;
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(uri);
+  
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+
+  kittySchema.methods.speak = function speak() {
+    const greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  };
+
+  const Kitten = mongoose.model('Kitten', kittySchema);
+
+  const kittens = await Kitten.find();
+  console.log(kittens);
+
+  const fluffyDocument = await Kitten.findOne({ name: /^fluff/ });
+  const fluffy = new Kitten(fluffyDocument);
+
+  console.log(fluffy.name);
+
+  fluffy.speak();
+}
+
+
+console.log('What else?');
